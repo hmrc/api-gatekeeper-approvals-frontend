cf99bb41ea41a34613a91b4ecb99680bf934ca4c
Pete Slater <pslater@equalexperts.com>
2022-01-14 13:10:41 +0000

    APIS-557 - Removing HelloWorld related code and dropping scoverage down to 75

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -2,7 +2,7 @@ import scoverage.ScoverageKeys._
 
 object ScoverageSettings {
   def apply() = Seq(
-    coverageMinimum := 80,
+    coverageMinimum := 75,
     coverageFailOnMinimum := true,
     coverageHighlighting := true,
     coverageExcludedPackages :=  Seq(
@@ -15,8 +15,7 @@ object ScoverageSettings {
       "com\\.kenshoo\\.play\\.metrics\\.*",
       ".*definition.*",
       ".*BuildInfo.*",
-      ".*javascript",
-      "uk.gov.hmrc.modules.common.services.EitherTHelper"
+      ".*javascript"
     ).mkString(";")
   )
 }
