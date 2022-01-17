700dbfdcb1b367080004dd09d14dd76540acfd00
Pete Slater <pslater@equalexperts.com>
2022-01-17 11:52:08 +0000

    APIS-5581 - Started passed page

--- app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/controllers/CheckAnswersThatPassedController.scala
+++ app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/controllers/CheckAnswersThatPassedController.scala
@@ -28,11 +28,16 @@ import uk.gov.hmrc.apigatekeeperapprovalsfrontend.controllers.actions.Applicatio
 import uk.gov.hmrc.apigatekeeperapprovalsfrontend.config.ErrorHandler
 import uk.gov.hmrc.apigatekeeperapprovalsfrontend.services.ApplicationActionService
 import uk.gov.hmrc.modules.submissions.services.SubmissionService
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.views.html.CheckAnswersThatPassedPage
+import scala.concurrent.Future.successful
+import uk.gov.hmrc.modules.submissions.domain.models._
+import uk.gov.hmrc.modules.submissions.domain.services.ActualAnswersAsText
+import cats.data.NonEmptyList
 
 object CheckAnswersThatPassedController {  
   case class AnswerDetails(question: String, answer: String)
-
-  case class ViewModel(applicationId: ApplicationId, appName: String, answers: List[AnswerDetails])
+  case class Section(heading: String, answerDetails: NonEmptyList[AnswerDetails] )
+  case class ViewModel(applicationId: ApplicationId, appName: String, sections: List[Section])
 }
 
 @Singleton
@@ -41,15 +46,60 @@ class CheckAnswersThatPassedController @Inject()(
   authConnector: AuthConnector,
   forbiddenHandler: ForbiddenHandler,
   mcc: MessagesControllerComponents,
+  checkAnswersThatPassedPage: CheckAnswersThatPassedPage,
   val errorHandler: ErrorHandler,
   val applicationActionService: ApplicationActionService,
   val submissionService: SubmissionService
 )(implicit override val ec: ExecutionContext) extends GatekeeperBaseController(strideAuthConfig, authConnector, forbiddenHandler, mcc) with ApplicationActions {
+
+  import CheckAnswersThatPassedController._
+
   def checkAnswersThatPassedPage(applicationId: ApplicationId): Action[AnyContent] = loggedInWithApplicationAndSubmission(applicationId) { implicit request =>
-    ???
+    def isPass(id: QuestionId): Boolean = {
+      request.markedAnswers.get(id).map(_ == Pass).getOrElse(false)
+    }
+
+    val groupedPassedQuestionsIds = 
+      request.submission.groups
+      .map(group =>
+        (
+          group.heading,
+          group.links
+            .flatMap(l => l.questions.map(_.question))
+            .filter(q => isPass(q.id))
+            .map(q => (q, request.answersToQuestions.get(q.id)))
+            .collect {
+              case (question, Some(answer)) => AnswerDetails(question.wording.value, ActualAnswersAsText(answer))
+            }
+        )
+      )
+      .collect {
+        case (heading, head::tail) => Section(heading, NonEmptyList.of(head, tail:_*))
+      }
+
+    successful(
+      Ok(
+        checkAnswersThatPassedPage(
+          CheckAnswersThatPassedController.ViewModel(
+            applicationId,
+            request.application.name,
+            groupedPassedQuestionsIds
+          )
+        )
+      )
+    )
   }
 
   def checkAnswersThatPassedAction(applicationId: ApplicationId): Action[AnyContent] = loggedInWithApplicationAndSubmission(applicationId) { implicit request =>
-    ???
+    def handleAction(action: String) = action match {
+      case "checked"          => Redirect(uk.gov.hmrc.apigatekeeperapprovalsfrontend.controllers.routes.ApplicationController.applicationPage(applicationId)) // TODO: Add actual route when implementing button actions
+      case "come-back-later"  => Redirect(uk.gov.hmrc.apigatekeeperapprovalsfrontend.controllers.routes.ApplicationController.applicationPage(applicationId)) // TODO: Add actual route when implementing button actions
+      case _                  => BadRequest(errorHandler.badRequestTemplate)
+    }
+
+    request.body.asFormUrlEncoded.getOrElse(Map.empty).get("submit-action") match {
+      case Some(value) => successful(value.headOption.fold(BadRequest(errorHandler.badRequestTemplate))(handleAction(_)))
+      case None => successful(BadRequest(errorHandler.badRequestTemplate))
+    }
   }
 }
