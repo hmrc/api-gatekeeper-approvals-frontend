791ecdaef0b4d3ecc5fe47757bc758640895cc06
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-01-25 11:18:24 +0000

    API-6431 Add new page for send new terms of use requested screen

--- app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/controllers/SendNewTermsOfUseController.scala
+++ app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/controllers/SendNewTermsOfUseController.scala
@@ -24,52 +24,58 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.apiplatform.modules.gkauth.services.StrideAuthorisationService
 import uk.gov.hmrc.apiplatform.modules.submissions.services.SubmissionService
 
-import uk.gov.hmrc.apigatekeeperapprovalsfrontend.config.ErrorHandler
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.config.{ErrorHandler, GatekeeperConfig}
 import uk.gov.hmrc.apigatekeeperapprovalsfrontend.domain.models.{ApplicationId, Standard}
 import uk.gov.hmrc.apigatekeeperapprovalsfrontend.services.ApplicationActionService
-import uk.gov.hmrc.apigatekeeperapprovalsfrontend.views.html.SendNewTermsOfUseConfirmPage
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.views.html.{SendNewTermsOfUseConfirmPage, SendNewTermsOfUseRequestedPage}
 
 object SendNewTermsOfUseController {
-  case class ViewModel(appName: String, applicationId: ApplicationId)
+  case class ViewModel(appName: String, applicationId: ApplicationId, applicationDetailsUrl: String)
 }
 
 @Singleton
 class SendNewTermsOfUseController @Inject() (
+    config: GatekeeperConfig,
     strideAuthorisationService: StrideAuthorisationService,
     mcc: MessagesControllerComponents,
     errorHandler: ErrorHandler,
     sendNewTermsOfUseConfirmPage: SendNewTermsOfUseConfirmPage,
+    sendNewTermsOfUseRequestedPage: SendNewTermsOfUseRequestedPage,
     val applicationActionService: ApplicationActionService,
     val submissionService: SubmissionService
   )(implicit override val ec: ExecutionContext
   ) extends AbstractApplicationController(strideAuthorisationService, mcc, errorHandler) {
 
-  def page(applicationId: ApplicationId): Action[AnyContent] = loggedInThruStrideWithApplicationAndSubmission(applicationId) { implicit request =>
+  def page(applicationId: ApplicationId): Action[AnyContent] = loggedInThruStrideWithApplication(applicationId) { implicit request =>
     request.application.access match {
       // Should only be uplifting and checking Standard apps
       case std: Standard =>
+        val gatekeeperApplicationUrl = s"${config.applicationsPageUri}/${applicationId.value}"
         successful(
           Ok(
             sendNewTermsOfUseConfirmPage(
               SendNewTermsOfUseController.ViewModel(
                 request.application.name,
-                applicationId
+                applicationId,
+                gatekeeperApplicationUrl
               )
             )
           )
         )
-      case _                                                        => successful(BadRequest(errorHandler.badRequestTemplate))
+      case _             => successful(BadRequest(errorHandler.badRequestTemplate))
     }
   }
 
-  def action(applicationId: ApplicationId): Action[AnyContent] = loggedInThruStrideWithApplicationAndSubmission(applicationId) { implicit request =>
-    successful(Ok(sendNewTermsOfUseConfirmPage(
+  def action(applicationId: ApplicationId): Action[AnyContent] = loggedInThruStrideWithApplication(applicationId) { implicit request =>
+    val gatekeeperApplicationUrl = s"${config.applicationsPageUri}/${applicationId.value}"
+    request.body.asFormUrlEncoded.getOrElse(Map.empty).get("invite-admins").flatMap(_.headOption) match {
+      case Some("yes") => successful(Ok(sendNewTermsOfUseRequestedPage(
               SendNewTermsOfUseController.ViewModel(
                 request.application.name,
-                applicationId
-              )
-            )
-          )
-        )
+                applicationId,
+                gatekeeperApplicationUrl
+              ))))
+      case _           => successful(Redirect(gatekeeperApplicationUrl))
+    }
   }
 }
