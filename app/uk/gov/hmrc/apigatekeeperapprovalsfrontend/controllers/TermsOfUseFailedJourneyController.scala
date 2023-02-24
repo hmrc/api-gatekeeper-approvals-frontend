fa2c3603b80e425871fce5e139d07478314a637a
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-02-24 15:59:04 +0000

    API-6433 New page to view warnings/fails for ToU

---
+++ app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/controllers/TermsOfUseFailedJourneyController.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.apigatekeeperapprovalsfrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future.successful
+
+import play.api.mvc.{MessagesControllerComponents, _}
+import uk.gov.hmrc.apiplatform.modules.gkauth.services.StrideAuthorisationService
+import uk.gov.hmrc.apiplatform.modules.submissions.domain.models._
+import uk.gov.hmrc.apiplatform.modules.submissions.domain.services.ActualAnswersAsText
+import uk.gov.hmrc.apiplatform.modules.submissions.services.SubmissionService
+
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.config.ErrorHandler
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.domain.models.{ApplicationId, State, SubmissionReview}
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.services.{ApplicationActionService, SubmissionReviewService}
+import uk.gov.hmrc.apigatekeeperapprovalsfrontend.views.html.TermsOfUseFailedListPage
+
+object TermsOfUseFailedJourneyController {
+  case class AnswerDetails(question: String, answer: String, status: Mark)
+
+  case class ViewModel(applicationId: ApplicationId, appName: String, answers: List[AnswerDetails], isDeleted: Boolean) {
+    lazy val hasFails: Boolean = answers.exists(_.status == Fail)
+    lazy val hasWarns: Boolean = answers.exists(_.status == Warn)
+
+    lazy val messageKey: String = if (hasFails) { if (hasWarns) "failsandwarns" else "failsonly" }
+    else "warnsonly"
+  }
+}
+
+@Singleton
+class TermsOfUseFailedJourneyController @Inject() (
+    strideAuthorisationService: StrideAuthorisationService,
+    mcc: MessagesControllerComponents,
+    errorHandler: ErrorHandler,
+    submissionReviewService: SubmissionReviewService,
+    termsOfUseFailedListPage: TermsOfUseFailedListPage,
+    val applicationActionService: ApplicationActionService,
+    val submissionService: SubmissionService
+  )(implicit override val ec: ExecutionContext
+  ) extends AbstractCheckController(strideAuthorisationService, mcc, errorHandler, submissionReviewService) {
+
+  import TermsOfUseFailedJourneyController._
+
+  def listPage(applicationId: ApplicationId) = loggedInThruStrideWithApplicationAndSubmission(applicationId) { implicit request =>
+    val appName   = request.application.name
+    val isDeleted = request.application.state.name == State.DELETED
+
+    val questionsAndAnswers: Map[Question, ActualAnswer] =
+      request.submission.latestInstance.answersToQuestions.map {
+        case (questionId, answer) => (request.submission.findQuestion(questionId) -> answer)
+      }
+        .collect {
+          case (q: Some[Question], a) => q.get -> a
+        }
+
+    val answerDetails = questionsAndAnswers.map {
+      case (question, answer) =>
+        AnswerDetails(
+          question.wording.value,
+          ActualAnswersAsText(answer),
+          request.markedAnswers.getOrElse(question.id, Pass)
+        )
+    }
+      .toList
+      .filter(_.status != Pass)
+
+    successful(
+      Ok(
+        termsOfUseFailedListPage(
+          ViewModel(
+            applicationId,
+            appName,
+            answerDetails,
+            isDeleted
+          )
+        )
+      )
+    )
+  }
+
+  def listAction(applicationId: ApplicationId): Action[AnyContent] = updateActionStatus(SubmissionReview.Action.CheckFailsAndWarnings)(applicationId)
+}
