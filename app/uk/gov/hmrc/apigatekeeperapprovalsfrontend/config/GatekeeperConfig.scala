5f87701363eb0f3f62d87ddd81410eab364e67dc
petekirby-ee <pete.kirby@digital.hmrc.gov.uk>
2023-04-19 09:27:40 +0100

    API-6754 Fix developers link in nav bar (#113)

--- app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/config/GatekeeperConfig.scala
+++ app/uk/gov/hmrc/apigatekeeperapprovalsfrontend/config/GatekeeperConfig.scala
@@ -18,7 +18,7 @@ package uk.gov.hmrc.apigatekeeperapprovalsfrontend.config
 
 case class GatekeeperConfig(gatekeeperBaseUrl: String) {
   lazy val applicationsPageUri = s"${gatekeeperBaseUrl}/api-gatekeeper/applications"
-  lazy val developersPageUri   = s"${gatekeeperBaseUrl}/api-gatekeeper/developers2"
+  lazy val developersPageUri   = s"${gatekeeperBaseUrl}/api-gatekeeper/developers"
   lazy val termsOfUsePageUri   = s"${gatekeeperBaseUrl}/api-gatekeeper/terms-of-use"
   lazy val emailsPageUri       = s"${gatekeeperBaseUrl}/api-gatekeeper/emails"
   lazy val pendingUri          = s"${gatekeeperBaseUrl}/api-gatekeeper/pending"
