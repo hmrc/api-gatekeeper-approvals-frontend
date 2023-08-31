467413479d80fe31b993e362d1b6fac5319fce5a
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-08-31 09:17:15 +0100

    API-7271 - Change to use clipboard API (#130)

--- app/assets/javascripts/copy-to-clip.js
+++ app/assets/javascripts/copy-to-clip.js
@@ -1,29 +1,5 @@
 function copyToClipboard(textToCopy) {
-  // navigator clipboard api needs a secure context (https)
-  if (navigator.clipboard && window.isSecureContext) {
-    // navigator clipboard api method'
-    return navigator.clipboard.writeText(textToCopy);
-  } else {
-    // text area method
-    var textArea = document.createElement("textarea");
-    textArea.value = textToCopy;
-    // make the textarea out of viewport
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-
-    try {
-      var successful = document.execCommand('copy');
-    } catch (e) {
-      // allow failure - still want to remove textArea
-      // test if we should even display the button later
-    }
-
-    textArea.remove();
-  }
+  navigator.clipboard.writeText(textToCopy);
 }
 
 function initCopyTextOnClick(clickedElementId, textSourceId) {
