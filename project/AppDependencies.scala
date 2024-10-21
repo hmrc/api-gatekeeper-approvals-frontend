b2da34387b5dda89349d6b0c6159c538ff20d69b
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-10-21 11:05:56 +0100

    API-7875 - Remove transitive deps now all is stable

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
   lazy val bootstrapPlayVersion = "9.0.0"
   lazy val mongoVersion = "2.1.0"
   val apiDomainVersion = "0.19.1"
-  val commonDomainVersion = "0.17.0"
   val appDomainVersion = "0.61.16"
 
   val compile = Seq(
@@ -15,7 +14,6 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "http-metrics"                       % "2.8.0",
     "org.typelevel"           %% "cats-core"                          % "2.10.0",
     "uk.gov.hmrc"             %% "internal-auth-client-play-30"       % "3.0.0",
-    "uk.gov.hmrc"             %% "api-platform-common-domain"         % commonDomainVersion,   // Override transitive dep
     "uk.gov.hmrc"             %% "api-platform-application-domain"    % appDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-api-domain"            % apiDomainVersion
   )
@@ -25,7 +23,6 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"                    % mongoVersion,
     "org.jsoup"                   %  "jsoup"                                      % "1.15.4",
     "org.mockito"                 %% "mockito-scala-scalatest"                    % "1.17.29",
-    "uk.gov.hmrc"                 %% "api-platform-common-domain-fixtures"        % commonDomainVersion,   // Override transitive dep
     "uk.gov.hmrc"                 %% "api-platform-application-domain-fixtures"   % appDomainVersion
   ).map(_ % "test")
 }
