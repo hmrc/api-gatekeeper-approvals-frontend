7eeb3b6fef448c3eac2c17032ff2abacb083eb17
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-03-11 13:59:32 +0000

    API-8268 - PR Bot fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  lazy val bootstrapPlayVersion = "9.7.0"
-  lazy val mongoVersion = "2.4.0"
+  lazy val bootstrapPlayVersion = "9.11.0"
+  lazy val mongoVersion = "2.5.0"
   val apiDomainVersion = "0.19.1"
-  val appDomainVersion = "0.72.0"
+  val appDomainVersion = "0.75.0"
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"         % bootstrapPlayVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"         % "11.11.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"         % "11.12.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                 % mongoVersion,
-    "uk.gov.hmrc"             %% "http-metrics"                       % "2.8.0",
+    "uk.gov.hmrc"             %% "http-metrics"                       % "2.9.0",
     "org.typelevel"           %% "cats-core"                          % "2.10.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30"       % "3.0.0",
+    "uk.gov.hmrc"             %% "internal-auth-client-play-30"       % "3.1.0",
     "uk.gov.hmrc"             %% "api-platform-application-domain"    % appDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-api-domain"            % apiDomainVersion
   )
