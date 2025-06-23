0bb1bc3cb5793f2cedc78a3ed9a4f06dc48d2b56
Tobias1087 <623674+Tobias1087@users.noreply.github.com>
2025-06-23 09:42:11 +0100

    APIS-7644 - Update for rebranding work

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,28 +1,29 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
   lazy val bootstrapPlayVersion = "9.11.0"
-  lazy val mongoVersion = "2.5.0"
-  val apiDomainVersion = "0.19.1"
-  val appDomainVersion = "0.75.0"
+  lazy val mongoVersion         = "2.5.0"
+  val apiDomainVersion          = "0.19.1"
+  val appDomainVersion          = "0.75.0"
+  val playfrontendVersion       = "12.6.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"         % bootstrapPlayVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"         % "11.12.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                 % mongoVersion,
-    "uk.gov.hmrc"             %% "http-metrics"                       % "2.9.0",
-    "org.typelevel"           %% "cats-core"                          % "2.10.0",
-    "uk.gov.hmrc"             %% "internal-auth-client-play-30"       % "3.1.0",
-    "uk.gov.hmrc"             %% "api-platform-application-domain"    % appDomainVersion,
-    "uk.gov.hmrc"             %% "api-platform-api-domain"            % apiDomainVersion
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"      % bootstrapPlayVersion,
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"      % playfrontendVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"              % mongoVersion,
+    "uk.gov.hmrc"       %% "http-metrics"                    % "2.9.0",
+    "org.typelevel"     %% "cats-core"                       % "2.10.0",
+    "uk.gov.hmrc"       %% "internal-auth-client-play-30"    % "3.1.0",
+    "uk.gov.hmrc"       %% "api-platform-application-domain" % appDomainVersion,
+    "uk.gov.hmrc"       %% "api-platform-api-domain"         % apiDomainVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                 %% "bootstrap-test-play-30"                     % bootstrapPlayVersion,
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"                    % mongoVersion,
-    "org.jsoup"                   %  "jsoup"                                      % "1.15.4",
-    "org.mockito"                 %% "mockito-scala-scalatest"                    % "1.17.29",
-    "uk.gov.hmrc"                 %% "api-platform-application-domain-fixtures"   % appDomainVersion
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"                   % bootstrapPlayVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30"                  % mongoVersion,
+    "org.jsoup"          % "jsoup"                                    % "1.15.4",
+    "org.mockito"       %% "mockito-scala-scalatest"                  % "1.17.29",
+    "uk.gov.hmrc"       %% "api-platform-application-domain-fixtures" % appDomainVersion
   ).map(_ % "test")
 }
