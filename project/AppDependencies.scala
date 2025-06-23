72ced451035b251dbefbcd25a12b87a314f18556
Tobias1087 <623674+Tobias1087@users.noreply.github.com>
2025-06-23 16:04:15 +0100

    APIS-7644 - PR Bot warnings

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,10 +2,10 @@ import sbt.*
 
 object AppDependencies {
 
-  lazy val bootstrapPlayVersion = "9.11.0"
-  lazy val mongoVersion         = "2.5.0"
+  lazy val bootstrapPlayVersion = "9.13.0"
+  lazy val mongoVersion         = "2.6.0"
   val apiDomainVersion          = "0.19.1"
-  val appDomainVersion          = "0.75.0"
+  val appDomainVersion          = "0.79.0"
   val playfrontendVersion       = "12.6.0"
 
   val compile = Seq(
